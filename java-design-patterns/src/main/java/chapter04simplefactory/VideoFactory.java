77d1bcc99fb544891515637ab4599b8a3eeb57d0
liang shan guang <1648266192@qq.com>
2019-04-22 22:44:31 +0800

    如何在UML中剔除Test代码

--- java-design-patterns/src/main/java/chapter04simplefactory/VideoFactory.java
+++ java-design-patterns/src/main/java/chapter04simplefactory/VideoFactory.java
@@ -7,4 +7,12 @@
 package chapter04simplefactory;
 
 public class VideoFactory {
+    public Video getVideo(String type) {
+        if ("java".equalsIgnoreCase(type)) {
+            return new JavaVideo();
+        } else if ("python".equalsIgnoreCase(type)) {
+            return new PythonVideo();
+        }
+        return null;
+    }
 }
