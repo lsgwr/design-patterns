67978f61264dc1e18e71a55da7079caff5e12f94
19920625lsg <1648266192@qq.com>
2020-04-29 22:55:35 +0800

    开始第5章 工厂模式啦

---
+++ java-design-patterns/src/main/java/第4到27章_23大设计模式/第5章_工厂模式/PythonVideo.java
@@ -0,0 +1,14 @@
+/***********************************************************
+ * @Description : 
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2019/4/22 08:14
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package 第4到27章_23大设计模式.第5章_工厂模式;
+
+public class PythonVideo extends Video {
+    @Override
+    public void produce() {
+        System.out.println("录制Python课程视频");
+    }
+}
---
+++ java-design-patterns/src/main/java/第4到27章_23大设计模式/第5章_工厂模式/VideoFactory.java
@@ -0,0 +1,11 @@
+/***********************************************************
+ * @Description : 视频生产的简单工厂
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2019/4/22 08:19
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package 第4到27章_23大设计模式.第5章_工厂模式;
+
+public abstract class VideoFactory {
+    public abstract Video getVideo();
+}
