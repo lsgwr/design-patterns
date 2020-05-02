e2ea2207d1e181d0fb1d21636c7d8957a7ef8e11
19920625lsg <1648266192@qq.com>
2020-05-02 13:10:45 +0800

    饿汉式的基本实现

--- java-design-patterns/src/main/java/第4到27章_23大设计模式/第08章_单例模式/饿汉式/V1基本实现/SingletonHungry.java
+++ java-design-patterns/src/main/java/第4到27章_23大设计模式/第08章_单例模式/饿汉式/V1基本实现/SingletonHungry.java
@@ -1,5 +1,6 @@
 /***********************************************************
- * @Description : 
+ * @Description : 饿汉时的基本实现，在类加载地时候就完成单例的初始化了
+ * 相比懒汉式虽然简单，但是存在浪费内存的风险
  * @author      : 梁山广(Liang Shan Guang)
  * @date        : 2020/5/2 13:02
  * @email       : liangshanguang2@gmail.com
@@ -7,4 +8,19 @@
 package 第4到27章_23大设计模式.第08章_单例模式.饿汉式.V1基本实现;
 
 public class SingletonHungry {
+    /**
+     * 类加载地时候就初始化，声明为final防止被二次赋值。static表明对所有实例有效，这也是单例模式的关键
+     */
+    private final static SingletonHungry singletonHungry = new SingletonHungry();
+
+    /**
+     * 构造器必须是私有地，防止外部创建类实例，这个务必记熟
+     */
+    private SingletonHungry() {
+
+    }
+
+    public static SingletonHungry getInstance() {
+        return singletonHungry;
+    }
 }
